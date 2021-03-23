d1251dc76e549b2c302c57c1cdf8139e1f578c61
Kristoffer Andersson <kod.kristoff@gmail.com>
2021-03-23 09:56:55 +0100

    feat: update

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,20 @@ use bevy::prelude::*;
 
 
 fn main() {
-    App::build().run()
+    App::build()
+        .add_startup_system(setup.system())
+        .add_plugins(DefaultPlugins)
+        .run();
+}
+
+fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.insert_resource(Materials {
+        head_material: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
+    });
+}
+
+struct SnakeHead;
+struct Materials {
+    head_material: Handle<ColorMaterial>,
 }
